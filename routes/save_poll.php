86d6d24af2b0eb47cd24cc8da7856f6a1b5145a2
Willg0115 <willg0115@outlook.com>
2023-03-26 17:31:04 -0700

    untested initialize quiz functionallity

--- routes/save_poll.php
+++ routes/save_poll.php
@@ -26,7 +26,7 @@ for ($i = 'A'; $i <= 'D'; $i++) {
 
 $answer = $_POST['correctAnswer'];
 
-$stmt = $conn->prepare("INSERT INTO $table_name (qname, content, qImage, optionA, optionB, optionC, optionD, answer) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt = $conn->prepare("INSERT INTO question (qname, content, qImage, optionA, optionB, optionC, optionD, answer) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("sssssss", $quiz, $question, $file, $options['A'], $options['B'], $options['C'], $options['D'], $answer);
 
 if ($stmt->execute()) {
