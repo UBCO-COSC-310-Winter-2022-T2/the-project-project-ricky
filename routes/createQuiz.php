a7f19a3c6409e9d7ce185527a14e4d34501523b1
Willg0115 <willg0115@outlook.com>
2023-03-26 17:10:17 -0700

    untested functinality to save a poll for a quiz

--- routes/createQuiz.php
+++ routes/createQuiz.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/createQuiz.css">
+
     <title>Quiz Creator</title>
     <script>
         function showForm() {
